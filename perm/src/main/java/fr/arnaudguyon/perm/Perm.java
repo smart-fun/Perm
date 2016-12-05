a16de7faa8c67e1b99c346ad6ee308b1420120f5
Arnaud Guyon <nonuguyon@gmail.com>
2016-12-05 13:41:08 +0100

    Added Javadoc.

--- perm/src/main/java/fr/arnaudguyon/perm/Perm.java
+++ perm/src/main/java/fr/arnaudguyon/perm/Perm.java
@@ -1,3 +1,18 @@
+/*
+    Copyright 2016 Arnaud Guyon
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ */
 package fr.arnaudguyon.perm;
 
 import android.app.Activity;
@@ -7,7 +22,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 
 /**
- * Created by aguyon on 02.12.16.
+ * Helper to Check or Request Android Permissions
  */
 
 public class Perm {
@@ -15,20 +30,24 @@ public class Perm {
     private Activity mActivity;
     private String mName;
 
+    /**
+     * Exception eventually thrown
+     */
     public class PermException extends RuntimeException {
         PermException(String message) {
             super(message);
         }
     }
 
-//  examples of androidPermission Strings
-//  Manifest.permission.CAMERA
-//  Manifest.permission.ACCESS_FINE_LOCATION
-
     private Perm() {
 
     }
 
+    /**
+     * Constructor
+     * @param activity an Activity
+     * @param androidPermission Android Permission like Manifest.permission.CAMERA or Manifest.permission.ACCESS_FINE_LOCATION
+     */
     public Perm(@NonNull Activity activity, @NonNull String androidPermission) {
         if (!androidPermission.startsWith("android.permission.")) {
             throw new PermException("Perm " + androidPermission + " is not an Android permission");
@@ -37,15 +56,27 @@ public class Perm {
         mName = androidPermission;
     }
 
+    /**
+     *
+     * @return true if the Permission is already Granted
+     */
     public boolean isGranted() {
         return (ContextCompat.checkSelfPermission(mActivity, mName) == PackageManager.PERMISSION_GRANTED);
     }
 
+    /**
+     *
+     * @return true if the Permission is already Denied
+     */
     public boolean isDenied() {
         return ((ContextCompat.checkSelfPermission(mActivity, mName) == PackageManager.PERMISSION_DENIED)
                 && ActivityCompat.shouldShowRequestPermissionRationale(mActivity, mName));
     }
 
+    /**
+     * Requests an Android permission. The response will be send to the onRequestPermissionsResult method of the mActivity
+     * @param requestCode user code that could be useful to differentiate several requests
+     */
     public void askPermission(int requestCode) {
         ActivityCompat.requestPermissions(mActivity, new String[]{mName}, requestCode);
     }
--- perm/src/main/java/fr/arnaudguyon/perm/PermResult.java
+++ perm/src/main/java/fr/arnaudguyon/perm/PermResult.java
@@ -1,3 +1,18 @@
+/*
+    Copyright 2016 Arnaud Guyon
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ */
 package fr.arnaudguyon.perm;
 
 import android.content.pm.PackageManager;
@@ -7,7 +22,7 @@ import static fr.arnaudguyon.perm.PermResult.Result.DENIED;
 import static fr.arnaudguyon.perm.PermResult.Result.GRANTED;
 
 /**
- * Created by aguyon on 02.12.16.
+ * Helper to analyse teh result of a Permission request
  */
 
 public class PermResult {
@@ -24,6 +39,11 @@ public class PermResult {
 
     }
 
+    /**
+     * Constructor, to be called from the onRequestPermissionsResult method of the Activity
+     * @param permissions list of permissions
+     * @param grantResults list of results
+     */
     public PermResult(String[] permissions, int[] grantResults) {
         if ((permissions == null) || (permissions.length == 0) || (grantResults == null) || (grantResults.length == 0)) {
             mResult = CANCELLED;
@@ -33,14 +53,26 @@ public class PermResult {
         }
     }
 
+    /**
+     *
+     * @return true if the permission has just been granted
+     */
     public boolean isGranted() {
         return (mResult == GRANTED);
     }
 
+    /**
+     *
+     * @return true if the permission has just been denied
+     */
     public boolean isDenied() {
         return (mResult == DENIED);
     }
 
+    /**
+     *
+     * @return true if the permission process has been cancelled
+     */
     public boolean isCancelled() {
         return (mResult == CANCELLED);
     }
